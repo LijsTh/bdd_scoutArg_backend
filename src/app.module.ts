fce2d91de691b69181939a14e99f254de07c5d2b
Juan Martin de la Cruz <jdelacruz@fi.uba.ar>
2024-11-25 15:07:18 -0300

    [FIX] Removed imports of modules that were deleted (photos)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,12 @@ import { Module } from '@nestjs/common';
 import { PlayersModule } from './domains/players/players.module';
 import { TeamsModule } from './domains/teams/teams.module';
 import { UsersModule } from './domains/users/users.module';
-import { TeamsPhotosModule } from './teams_photos/teams_photos.module';
-import { UsersPhotosModule } from './users_photos/users_photos.module';
-import { PlayersPhotosModule } from './players_photos/players_photos.module';
 
 @Module({
   imports: [
     UsersModule,
     PlayersModule,
     TeamsModule,
-    TeamsPhotosModule,
-    UsersPhotosModule,
-    PlayersPhotosModule,
   ],
 })
 export class AppModule {}
